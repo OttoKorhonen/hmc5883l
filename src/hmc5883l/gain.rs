508795db4d70eb9da974c0826fcd5b4ae9f23b19
Otto Korhonen <otto@xd.fi>
2025-03-19 14:34:18 +0200

    fixed logic in code and now return values make sense

--- src/hmc5883l/gain.rs
+++ src/hmc5883l/gain.rs
@@ -1,6 +1,6 @@
 
 pub enum Gain {
-    High = 0b1110_0000,
-    Default = 0b0010_0000,
-    Low = 0b0000_0000 
+    High = 0b0000_1110,   // GN2 = 0, GN1 = 1, GN0 = 1 (2.5 Ga)
+    Default = 0b0000_0010, // GN2 = 0, GN1 = 0, GN0 = 1 (1.3 Ga, oletus)
+    Low = 0b0000_0000,     // GN2 = 0, GN1 = 0, GN0 = 0 (0.88 Ga)
 }
--- src/hmc5883l/hmc5883l.rs
+++ src/hmc5883l/hmc5883l.rs
@@ -3,7 +3,6 @@ use core::marker::PhantomData;
 use core::{fmt, i32};
 use embedded_hal::i2c::I2c as HalI2c;
 use esp_hal::delay::Delay;
-use esp_println::println;
 use heapless::Vec;
 use libm::atan2f;
 
@@ -31,72 +30,78 @@ where
         }
     }
 
+    fn get_x_register(&mut self) -> [u8; 2] {
+        let cmd = [0x03];  // X-akselin MSB
+        let mut response = [0u8; 6]; // Lue kaikki akselidatat samalla
+        self.i2c.write_read(self.device_address, &cmd, &mut response).unwrap();
+    
+        [response[0], response[1]] // Palauta vain X:n MSB ja LSB
+    }
+
+    fn get_y_register(&mut self) -> [u8; 2] {
+        let cmd = [0x07];  // X-akselin MSB
+        let mut response = [0u8; 6]; // Lue kaikki akselidatat samalla
+        self.i2c.write_read(self.device_address, &cmd, &mut response).unwrap();
+    
+        [response[0], response[1]] // Palauta vain X:n MSB ja LSB
+    }
+
     ///take single measure
-    pub fn single_measurement(&mut self, gain: Gain) -> [u8; 6] {
+    pub fn single_measurement(&mut self, gain: Gain) -> f32 {
         let delay = Delay::new();
         self.cra();
         self.crb(gain);
         self.mode_register(Mode::Single);
         delay.delay_millis(6);
 
-        let cmd = [0x06];
-        let mut response = [0u8; 6];
+        let x = self.get_x_register();
+        let y = self.get_y_register();
 
-        self.i2c
-        .write_read(self.device_address, &cmd, &mut response)
-        .unwrap();
-
-        println!("res {:?}", response);
+        let response = [x[0], x[1], y[0], y[1]];
 
         let gauss_values = self.measure_gauss(&response);
 
-        self.atan2_custom(gauss_values[1], gauss_values[2]);
+        let angle = self.atan2_custom(gauss_values[1], gauss_values[2]);
 
-        self.i2c.write(self.device_address, &[0x3C, 0x03]).unwrap();
-
-
-        response
+        angle
     }
 
-    //take continuous measurement
-    pub fn continuous_measure(&mut self, gain: Gain) {
+
+    ///take continuous measurement
+    pub fn continuous_measure(&mut self, gain: Gain) -> f32 {
         let delay = Delay::new();
         self.cra();
         self.crb(gain);
         self.mode_register(Mode::Continuous);
         delay.delay_millis(6);
 
-        let cmd = [0x06];
-        let mut response = [0u8; 6];
-
         loop {
-            self.i2c
-                .write_read(self.device_address, &cmd, &mut response)
-                .unwrap();
 
+            let x = self.get_x_register();
+            let y = self.get_y_register();
+
+            let response = [x[0], x[1], y[0], y[1]];
             let gauss_values = self.measure_gauss(&response);
 
-            self.atan2_custom(gauss_values[1], gauss_values[2]);
+            delay.delay_millis(67);
 
-            self.i2c.write(self.device_address, &[0x3C, 0x03]).unwrap();
-
-            delay.delay_millis(70);
+            return self.atan2_custom(gauss_values[0], gauss_values[1]);
         }
     }
 
     //East-Counterclockwise Convention y,x
     fn atan2_custom(&self, y: f32, x: f32) -> f32 {
-        let mut angle = atan2f(y, x) * (360.0 / PI);
+        let mut angle = atan2f(y, x) * (180.0 / PI);
 
         if angle < 0.0 {
             angle += 360.0;
         }
-        //println!("Angle: {:.2}°", angle);
+        // println!("Angle: {:.2}°", angle);
         angle
     }
 
-    fn measure_gauss(&self, raw_data: &[u8; 6]) -> Vec<f32, 3> {
-        let mut gauss_values: Vec<f32, 3> = Vec::new();
+    fn measure_gauss(&self, raw_data: &[u8; 4]) -> Vec<f32, 2> {
+        let mut gauss_values: Vec<f32, 2> = Vec::new();
 
         for chunk in raw_data.chunks(2) {
             let mut direction = ((chunk[0] as i32) << 8) | (chunk[1] as i32);
@@ -107,25 +112,25 @@ where
             }
 
             //value converted into gauss
-            let gauss = direction as f32 / 1090.0; // 1090 on muutettava vastaamaan gainia
+            let gauss = direction as f32 / 230.0; // 1090 on muutettava vastaamaan gainia
 
             gauss_values.push(gauss).unwrap();
         }
         gauss_values
     }
 
-fn cra(&mut self) {
-    let cmd = [0x00, 0b0111_0000]; // Configuration Register A (0x00)
-    self.i2c.write(self.device_address, &cmd).unwrap();
-}
+    fn cra(&mut self) {
+        let cmd = [0x00, 0b0011_0000]; // Configuration Register A (0x00)
+        self.i2c.write(self.device_address, &cmd).unwrap();
+    }
 
-fn crb(&mut self, gain: Gain) {
-    let cmd = [0x01, gain as u8]; // Configuration Register B (0x01)
-    self.i2c.write(self.device_address, &cmd).unwrap();
-}
+    fn crb(&mut self, gain: Gain) {
+        let cmd = [0x01, gain as u8]; // Configuration Register B (0x01)
+        self.i2c.write(self.device_address, &cmd).unwrap();
+    }
 
-fn mode_register(&mut self, mode: Mode) {
-    let cmd = [0x02, mode as u8]; // Mode Register (0x02)
-    self.i2c.write(self.device_address, &cmd).unwrap();
-}
+    fn mode_register(&mut self, mode: Mode) {
+        let cmd = [0x02, mode as u8]; // Mode Register (0x02)
+        self.i2c.write(self.device_address, &cmd).unwrap();
+    }
 }
