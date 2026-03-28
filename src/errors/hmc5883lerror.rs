6ee572b384113ff530c68ee8cb1e3a84304075d0
Otto Korhonen <otto@xd.fi>
2026-03-28 20:04:51 +0200

    added display amd from traits for hmc5883lerror enum

--- src/errors/hmc5883lerror.rs
+++ src/errors/hmc5883lerror.rs
@@ -1,8 +1,23 @@
+
+use core::fmt::{Debug, Display, Formatter};
 use serde::Serialize;
+
 #[derive(Debug, Serialize)]
-pub enum Hmc5883lError<E>{
-    I2cError(E)
+pub enum Hmc5883lError<E> {
+    I2cError(E),
 }
+
+impl<E> Display for Hmc5883lError<E>
+where
+    E: Debug,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Self::I2cError(E) => write!(f, "I2c error: {}", E),
+        }
+    }
+}
+impl<E> core::error::Error for Hmc5883lError<E> where E: Debug {}
 
 impl<E> From<E> for Hmc5883lError<E> {
     fn from(e: E) -> Self {
